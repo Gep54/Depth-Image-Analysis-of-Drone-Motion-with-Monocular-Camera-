1f2661c5f51aea1f7811a544f1c0e35290726bed
vberan <vaclav.beran@creativedock.com>
2026-03-28 12:37:14 +0100

    commit initial

---
+++ camera_calibration.py
@@ -0,0 +1,46 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+def calibrate_camera_chessboard():
+    # termination criteria
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((7 * 7, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:7, 0:7].T.reshape(-1, 2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane.
+
+    images = glob.glob('obrazky/sachovnice/*.jpg')
+    size = len(images)
+    counter = 1
+    for fname in images:
+        print(f"Scanning image number: {counter} of {size}")
+        counter = counter + 1
+        img = cv.imread(fname)
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+        # Find the chess board corners
+        ret, corners = cv.findChessboardCorners(gray, (7, 7), None)
+
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+
+            corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners2)
+
+            # Draw and display the corners
+            # cv.drawChessboardCorners(img, (7, 6), corners2, ret)
+            # cv.namedWindow('img', cv.WINDOW_NORMAL)
+            # cv.imshow('img', img)
+            # cv.waitKey(1000)
+
+    cv.destroyAllWindows()
+
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    return ret, mtx, dist, rvecs, tvecs
+
---
+++ mymatch.py
@@ -0,0 +1,86 @@
+import cv2 as cv
+import pandas as pd
+
+
+def edge_preprocess(image):
+    # Convert to grayscale
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    edges = cv.Canny(gray, threshold1=50, threshold2=150)
+
+    return edges
+
+def features_ORB(edges):
+    # ORB feature detector
+    orb = cv.ORB_create()
+
+    # Detect keypoints and descriptors
+    keypoints, descriptors = orb.detectAndCompute(edges, None)
+    return keypoints, descriptors
+
+def features_SIFT(edges):
+    # Create SIFT detector
+    sift = cv.SIFT_create()
+
+    # Detect keypoints and descriptors
+    keypoints, descriptors = sift.detectAndCompute(edges, None)
+    return keypoints, descriptors
+
+def features_KAZE(edges):
+    kaze = cv.KAZE_create()
+
+    # Detect keypoints and descriptors
+    keypoints, descriptors = kaze.detectAndCompute(edges, None)
+    return keypoints, descriptors
+
+def match(descriptors1, descriptors2):
+    # Match features using BFMatcher
+    bf = cv.BFMatcher(cv.NORM_L2, crossCheck=True)
+    matches = bf.match(descriptors1, descriptors2)
+
+    # Sort matches by distance (lower is better)
+    matches = sorted(matches, key=lambda x: x.distance)
+    return matches
+
+
+def visualize_images_matched_features(image1, image2, keypoints1, keypoints2, matches):
+    # Draw the top matches
+    matched_image = cv.drawMatches(image1, keypoints1, image2, keypoints2, matches[:50], None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    # Display the result
+    cv.namedWindow('Matched Features', cv.WINDOW_NORMAL)
+    cv.imshow('Matched Features', matched_image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+def save_images_matched_features(image1, image2, keypoints1, keypoints2, matches, name='matchedFeatures.jpg'):
+    # Draw the top matches
+    matched_image = cv.drawMatches(image1, keypoints1, image2, keypoints2, matches[:50], None,
+                                   flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    # Display the result
+    cv.imwrite(name, matched_image)
+
+def matches_to_df(matches, keypoints1, keypoints2):
+    # Extract matched feature coordinates
+    matched_features = []
+    for match in matches:
+        # Query and train indices
+        idx1 = match.queryIdx
+        idx2 = match.trainIdx
+
+        # Get keypoint coordinates
+        x1, y1 = keypoints1[idx1].pt  # Coordinates in image1
+        x2, y2 = keypoints2[idx2].pt  # Coordinates in image2
+
+        # Append to list
+        matched_features.append({
+            "Image1_X": x1,
+            "Image1_Y": y1,
+            "Image2_X": x2,
+            "Image2_Y": y2,
+            "Distance": match.distance
+        })
+
+    # Convert to Pandas DataFrame
+    df = pd.DataFrame(matched_features)
+    return df
